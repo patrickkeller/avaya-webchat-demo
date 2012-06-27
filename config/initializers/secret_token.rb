cee85ac608fabd3e39315b3f4cd15f87340b776c
Patrick Keller <byteclic@gmail.com>
2012-06-27 16:34:37 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AvayaWebchatDemo::Application.config.secret_token = 'b864dccd4a2ded8e9a0b71f8620ae114c9a1c7bec15195faaf3da9a047f8e649eed20ae53310162a7245063a93e04c125d3341510b5670f8779f45a1c6fc0f86'
